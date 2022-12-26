568a02ad19658db17b5dc4aa51b05b738b702671
Bayarkhuu13 <115783789+Bayarkhuu13@users.noreply.github.com>
2022-12-26 17:38:39 +0800

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,56 @@
+// // let age = prompt("What is your age?")
+
+// // // console.log(age)
+
+// // if(age >= 18) {
+// //     console.log('Та насанд хүрсэн байна')
+ // } else {
+ //     console.log('Та хүүхдээрээ байна');
+// }
+
+
+// let username = prompt("What is your username?")
+
+// const date = new Date()
+// const time = date.getHours()
+
+// console.log(username)
+
+// if (time<5) {
+//     alert('Good Morning' + " " +username)
+// } else if (time<16) {
+//     alert('Good Afternoon' + " " +username)
+// } else {
+//     alert('Good Evening'+ " " +username)
+// }
+
+
+// let bat = 67
+// let dumd = 59
+// let tulga = 80
+// let nomin = 95
+// let togsgerel = 99
+
+// if(let>60) {
+
+// }
+
+// let age = 21
+// let messega = age >= 18 ? 'Та насанд хүрсэн байна' : 'Та хүүхдээрээ байна' 
+// console.log(messega)
+
+// const number = prompt('Number')
+
+// if(number % 2 == 0 ) {
+//     alert('odd')
+// } else {
+//     alert('even')
+// }
+
+// console.log(number)
+
+const year = prompt("Year?")
+
+if(year % 4 == 0 ) {
+    aler
+}
